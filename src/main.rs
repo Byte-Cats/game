d6b862819e046f213fe5c58d401cacc1dd859c2d
Byte Cats <devin@bytecats.codes>
2022-10-03 17:36:54 -0400

    Graphical Display window

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world!");
-}
-
+pub const CLEAR_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const RESOLUTION: f32 = 16.0/9.0;
 
 fn main() {
+    let height: f32 = 600.0;
     App::new()
-        .add_system(hello_world)
-        .run();
+        .insert_resource(ClearColor(CLEAR_COLOR))
+        .insert_resource(WindowDescriptor {
+            title: "Byte Cats".to_string(),
+            present_mode: bevy::window::PresentMode::AutoVsync,
+            width: height * RESOLUTION,
+            height,
+            resizable: false,
+            ..Default::default()
+        })
+        // .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins).run();
 }
 
+//
+// fn spawn_camera(mut commands: Commands) {
+//     let mut camera =
+//     camera.orthographic_projection.top = 1.0;
+//     camera.orthographic_projection.bottom = -1.0;
+//     camera.orthographic_projection.left = -1.0 *RESOLUTION;
+//     camera.orthographic_projection.right = 1.0 *RESOLUTION;
+//
+//     //commands.spawn_bundle(camera);
+// }
