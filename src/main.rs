1503c7f223b4d46e8f06cfbeec2644228df41d26
Zastian Pretorius <zastian00@gmail.com>
2022-10-04 23:25:11 +0100

    implemented drp

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
+use bevy_discord_presence::config::{RPCConfig, RPCPlugin};
 
 pub const CLEAR_COLOR: Color = Color::rgb(0.1, 0.1, 0.1);
-pub const RESOLUTION: f32 = 16.0/9.0;
+pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
     let height: f32 = 600.0;
@@ -16,7 +17,12 @@ fn main() {
             ..Default::default()
         })
         // .add_startup_system(spawn_camera)
-        .add_plugins(DefaultPlugins).run();
+        .add_plugins(DefaultPlugins)
+        .add_plugin(RPCPlugin(RPCConfig {
+            app_id: 965125975941709834,
+            show_time: true,
+        }))
+        .run();
 }
 
 //
